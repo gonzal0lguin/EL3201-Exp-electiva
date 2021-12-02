433a4c10a1f712ecca9908f01a9fee0e52ec0a7e
gonzal0lguin <golguinm@gmail.com>
2021-12-02 18:23:33 -0300

    Escribe main de RPI

--- scripts/RPIclass.py
+++ scripts/RPIclass.py
@@ -1,21 +1,33 @@
 from time import sleep
 import RPi.GPIO as io
+from RPi.GPIO import LOW, HIGH
 from picamera import PiCamera
 
 class Camera(object):
     def __init__(self):
         self.cam = PiCamera()
         self.cam.resolution = (1024, 768)
+        self.pins = [26, 19, 13, 6]
+        self.init_leds()
+        self.path = "path"
 
-    def flash_enviroment(self):
-        # TODO: 'inicializar luces led que iluminen el ambiente para que las
-        #  fotos sean consistentes y haya la menor cantidad de errores'
+    def init_leds(self):
+        io.setmode(io.BCM)
+        io.setwarnings(False)
+        for i in range(len(self.pins)):
+            io.setup(self.pins[i], io.OUT, initial=io.LOW)
 
-        pass
+    def cool_waiting_flash(self):
+        for i in range(len(self.pins)):
+            io.output(self.pins[i], io.HIGH)
+            sleep(0.3)
+            io.output(self.pins[i], io.LOW)
+            sleep(0.3)
+
+    def flash_enviroment(self, mode):        
+        for i in range(len(self.pins)):
+            io.output(self.pins[i], mode)
 
     def capture(self):
-        # TODO: 'secuencia de captura de la foto y guardado, primero se
-        #  ilumina el mabiente y luego se procede con la foto'
-
-        self.flash_enviroment()
+        # TODO: 'secuencia de captura de la foto y guardado'
         pass
--- scripts/main.py
+++ scripts/main.py
@@ -1,27 +1,22 @@
-#from scripts.RPIclass import Camera
+from scripts.RPIclass import Camera
 
-
-# TODO: 'incluir todos los parametros que falten y definir la secuancia del
-#  trabajo, la idea esq el programa sea interactivo'
-
-
-#picam = Camera()
-
-
+picam = Camera()
 
 
 if __name__ == "__main__":
-    # secuancia aca
-
+    picam.cool_waiting_flash()
+    picam.flash_enviroment(io.HIGH)
     print('Hola! Porfavor, acerca tu imagen a la zona iluminada por la camara')
     input('Presiona "enter" para tomar la foto')
 
+    picam.capture()
     #Todo: 'aca la secuancia de toma de la foto, una vez que se obtiene se debe
     # mostrar un preview y preguntar si está bien, si no dar opciones para
     # corregir lo que esté mal'
 
 
-
     #### luego de que tod o este bien se uarda la imagen y se dice:
-
+    path = picam.path
     print('Perfecto! la imagen está guardada, revisa el archivo --archivo--')
+    print('En una terminal nativa copia el siguiente comando:')
+    print('scp /home/pi/EL3201-Exp-electiva/scripts/photos/{} path/to/location/{}'.format(path))
