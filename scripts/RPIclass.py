e1372f8c93d09c4bbc5a7707328ace5dd8e48909
gonzal0lguin <golguinm@gmail.com>
2021-12-02 16:45:29 -0300

    update funcs

--- scripts/RPIclass.py
+++ scripts/RPIclass.py
@@ -1,11 +1,27 @@
 from time import sleep
 import RPi.GPIO as io
+from RPi.GPIO import LOW, HIGH
 from picamera import PiCamera
 
 class Camera(object):
     def __init__(self):
         self.cam = PiCamera()
         self.cam.resolution = (1024, 768)
+        self.pins = [26, 19, 13, 6]
+        self.init_leds()
+
+    def init_leds(self):
+        io.setmode(io.BCM)
+        io.setwarnings(False)
+        for i in range(len(self.pins)):
+            io.setup(pins[i], io.OUT, initial=io.LOW)
+
+    def cool_waiting_flash(self):
+        for i in range(len(pins)):)
+            io.output(pins[i], io.HIGH)
+            sleep(0.3)
+            io.output(pins[i], io.LOW)
+            sleep(0.3)
 
     def flash_enviroment(self):
         # TODO: 'inicializar luces led que iluminen el ambiente para que las
@@ -15,5 +31,15 @@ class Camera(object):
 
     def capture(self):
         # TODO: 'secuencia de captura de la foto y guardado'
-
         pass
+
+
+cam = Camera()
+cam.cool_waiting_flash
+
+
+
+
+
+
+# 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
