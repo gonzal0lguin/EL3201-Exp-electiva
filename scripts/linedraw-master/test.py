4af4bf0efb3b86a4eae1ed94d99acd3744600232
gonzal0lguin <golguinm@gmail.com>
2021-12-03 22:39:57 -0300

    Añade codigo de linedraw

--- scripts/linedraw-master/test.py
+++
@@ -1,5 +0,0 @@
-import linedraw
-lines = linedraw.sketch("/Users/gonzalolguin/Desktop/testlindraw.jpg")  # return list of polylines, eg.
-                                            # [[(x,y),(x,y),(x,y)],[(x,y),(x,y),...],...]
-                                            
-linedraw.visualize(lines)    
